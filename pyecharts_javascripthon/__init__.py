e59fef30cb0af101e5878f26ea16235a46243b02
chfw <wangc_2011@hotmail.com>
2018-03-28 22:32:10 +0100

    :sparkles: provide dummy objects for javascripting

--- pyecharts_javascripthon/__init__.py
+++ pyecharts_javascripthon/__init__.py
@@ -3,3 +3,15 @@ from pyecharts_javascripthon._version import __version__
 from pyecharts_javascripthon._version import __author__
 
 from pyecharts_javascripthon.compiler import Python2Javascript
+
+# Dummy functions to make python compiler blind
+def Date(*_):
+    pass
+
+
+class Document:
+    pass
+
+
+class window:
+    pass
