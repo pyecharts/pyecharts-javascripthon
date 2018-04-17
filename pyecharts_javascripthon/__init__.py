bb2c6960336162d1af0e14f925300a27a629d929
chfw <wangc_2011@hotmail.com>
2018-04-18 00:23:12 +0100

    :bug: fix version check and setup.py

--- pyecharts_javascripthon/__init__.py
+++ pyecharts_javascripthon/__init__.py
@@ -3,7 +3,9 @@ import sys
 from pyecharts_javascripthon._version import __version__
 from pyecharts_javascripthon._version import __author__
 
-if PY35 = sys.version_info[:2] >= (3, 5):
+PY35_ABOVE = sys.version_info[:2] >= (3, 5)
+
+if PY35_ABOVE:
     from pyecharts_javascripthon.compiler import Python2Javascript
 else:
     from pyecharts_javascripthon.complier_client import Python2Javascript
