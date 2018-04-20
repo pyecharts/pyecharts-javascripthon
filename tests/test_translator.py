9657667a9051dc37ab90b4f513cc95ee188e7a48
chfw <wangc_2011@hotmail.com>
2018-04-20 19:09:12 +0100

    :microscope: more test coverage

--- tests/test_translator.py
+++ tests/test_translator.py
@@ -1,3 +1,7 @@
+import datetime
+from nose.tools import eq_, raises
+from textwrap import dedent
+
 from pyecharts_javascripthon.api import TRANSLATOR
 
 
@@ -22,3 +26,28 @@ def test_basic_usage():
     snippet = TRANSLATOR.translate(source2)
     assert 'function my_fmt' in snippet.function_snippet
     assert '"my_fmt"' not in snippet.option_snippet
+
+
+def test_datetime():
+    a_date = datetime.date(2018, 4, 20)
+    a_date_time = datetime.datetime(2018, 4, 20, 19, 2, 30)
+    option = {
+        'date': a_date,
+        'datetime': a_date_time
+    }
+    expected = """
+    {
+        "date": "2018-04-20",
+        "datetime": "2018-04-20T19:02:30"
+    }
+    """
+
+    actual = TRANSLATOR.translate(option)
+    eq_(actual.option_snippet, dedent(expected).strip())
+
+
+@raises(Exception)
+def test_time():
+    a_time = datetime.time(19, 2, 30)
+    option = {'time': a_time}
+    TRANSLATOR.translate(option)
