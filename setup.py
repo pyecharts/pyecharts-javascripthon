6f71fbe6811243117cb22adde110ce50a130d755
chfw <wangc_2011@hotmail.com>
2018-04-21 23:57:28 +0100

    :egg: :ferris_wheel: release 0.0.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ DESCRIPTION = (
     'Embeded Python functions in pyecharts'
 )
 URL = 'https://github.com/pyecharts/pyecharts-javascripthon'
-DOWNLOAD_URL = '%s/archive/0.0.1.tar.gz' % URL
+DOWNLOAD_URL = '%s/archive/0.0.2.tar.gz' % URL
 FILES = ['README.rst', 'CHANGELOG.rst']
 KEYWORDS = [
     'python'
@@ -48,8 +48,8 @@ EXTRAS_REQUIRE = {
 # You do not need to read beyond this line
 PUBLISH_COMMAND = '{0} setup.py sdist bdist_wheel upload -r pypi'.format(
     sys.executable)
-GS_COMMAND = ('gs pyecharts-javascripthon v0.0.1 ' +
-              "Find 0.0.1 in changelog for more details")
+GS_COMMAND = ('gs pyecharts-javascripthon v0.0.2 ' +
+              "Find 0.0.2 in changelog for more details")
 NO_GS_MESSAGE = ('Automatic github release is disabled. ' +
                  'Please install gease to enable it.')
 UPLOAD_FAILED_MSG = (
